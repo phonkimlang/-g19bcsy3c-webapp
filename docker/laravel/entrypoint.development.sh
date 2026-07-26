0bc1bf18f3b34d1d137cd4d90fcb1632572a66ed
phonkimlang <phunkemlang2021@gmail.com>
2026-07-26 20:52:40 +0700

    Session 3.7 Octane & RoadRunner

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -3,10 +3,10 @@ set -e
 
 # composer install
 # wait $!
+# npm install
+# wait $!
 php artisan key:generate
 wait $!
 php artisan migrate
 wait $!
-php artisan serve --host=0.0.0.0 --port=8000 &
-
-php artisan queue:work --tries=3 
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
